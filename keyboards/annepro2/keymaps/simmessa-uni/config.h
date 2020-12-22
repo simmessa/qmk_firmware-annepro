613007c0acd2dded300558d485671659c93b019a
Simone Messaggi <simone.messaggi@mia-platform.eu>
2020-12-22 12:30:29 +0100

    Avoid warnings

--- keyboards/annepro2/keymaps/simmessa-uni/config.h
+++ keyboards/annepro2/keymaps/simmessa-uni/config.h
@@ -1,17 +1,1 @@
-#pragma once
-
-#include "config_common.h"
-
 #define TAPPING_TERM 200
-
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0x0000  // "DZ"
-#define PRODUCT_ID      0x0000
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    Sim
-#define PRODUCT         AnnePro2
-#define DESCRIPTION     AnnePro2 Keyboard by Simmessa with unicode and tap dance
-
-/* key matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 15
